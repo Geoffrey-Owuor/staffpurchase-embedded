0ab536ebddd59234a72d40bcbd89d05dd86e9c21
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-08-10 15:16:38 +0300

    calendar skeleton size edit

--- components/skeletons/ApprovalCardsSkeleton.jsx
+++ components/skeletons/ApprovalCardsSkeleton.jsx
@@ -4,7 +4,7 @@ export default function ApprovalCardsSkeleton() {
       {[1, 2, 3].map((i) => (
         <div
           key={i}
-          className="relative h-39 overflow-hidden rounded-2xl border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-800"
+          className="relative h-37 overflow-hidden rounded-2xl border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-800"
         >
           <div className="shimmer absolute inset-0" />
         </div>
